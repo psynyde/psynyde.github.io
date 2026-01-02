bf65741801d82f3df7767d0076f727202f06fde5
psynyde <pxynyde@proton.me>
2026-01-02 21:32:25 +0600

    deploy

--- src/config.ts
+++ src/config.ts
@@ -30,7 +30,7 @@ export const themeConfig: ThemeConfig = {
   // POST SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   post: {
     readingTime: true, // Show reading time in posts
-    toc: true, // Show table of contents (when there is enough page width)
+    toc: false, // Show table of contents (when there is enough page width)
     imageViewer: true, // Enable image viewer
     copyCode: true, // Enable copy button in code blocks
     linkCard: true // Enable link card
